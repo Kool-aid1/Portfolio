eab95d33e643c8266157186d759f7e646143c07b
Kenny <67345281+Kool-aid1@users.noreply.github.com>
2024-08-09 00:21:16 -0700

    add overflow scroll for navbar bc lazy to change it rn

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 
 function Navbar() {
-
   return (
     <div>
       <header className="bg-zinc-400 h-16 fixed top-0 w-full z-10">
-        <nav className="h-full flex items-center justify-between px-6 border-b border-gray-500 whitespace-nowrap">
+        <nav className="h-full flex items-center justify-between px-6 border-b border-gray-500 overflow-x-auto whitespace-nowrap">
           <div className="flex items-center flex-shrink-0">
             <p className="font-bold text-lg p-4"> Kenny Ly </p>
           </div>
