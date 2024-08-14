9b7165317016474752a0165fc030491ad0676733
Kenny <67345281+Kool-aid1@users.noreply.github.com>
2024-08-14 15:34:26 -0700

    added social icons w/ links

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import myPhoto from "./images/kennyPFP.jpg";
+import github from "./images/github-logo.png";
+import linkedin from "./images/linkedin-logo.png";
 
 function AboutMe() {
   return (
@@ -18,7 +20,7 @@ function AboutMe() {
                 className="rounded-lg shadow-lg object-cover h-full w-full md:w-auto md:h-auto"
               />
             </div>
-            <div className="p-6 md:w-1/2 flex justify-center items-center">
+            <div className="p-6 md:w-1/2 flex flex-col justify-center items-center">
               <p className="text-lg leading-relaxed">
                 &emsp;Hello, my name is Kenny Ly and I am a recent graduate at
                 CSUSM with a BS in Software Engineering. I enjoy the different
@@ -29,6 +31,27 @@ function AboutMe() {
                 such as sudoku and minesweeper. To find patterns to solve
                 problems makes the game more enticing to me.
               </p>
+              <div className="mt-4 flex flex-row">
+                <a href="https://github.com/Kool-aid1" className="mt-4 p-2">
+                  <img
+                    src={github}
+                    alt="githublogo"
+                    href="https://github.com/Kool-aid1"
+                    className="rounded-sm w-10 h-10"
+                  />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/kennytly23/"
+                  className="mt-4 p-2"
+                >
+                  <img
+                    src={linkedin}
+                    alt="linkedinlogo"
+                    href="https://www.linkedin.com/in/kennytly23/"
+                    className="rounded-sm w-10 h-10"
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
